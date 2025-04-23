4b73401bb516e53314e98159268932cfcd129e82
tohirsolehudin47 <45795972+tohirsolehudin47@users.noreply.github.com>
2025-04-23 10:06:27 +0700

    Add includes/footer.php 1745377587623

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+</main>
+    <footer class="bg-white shadow p-4 text-center text-gray-600 text-sm">
+        &copy; <?php echo date('Y'); ?> Qurban Reseller Affiliate. All rights reserved.
+    </footer>
+</body>
+</html>
